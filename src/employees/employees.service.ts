7463e2fe167d79826c0805cb8fe25e587f6eaead
Abdullahil Ashik Md Arefin <abdullah.ashik.arefin@gmail.com>
2025-07-31 00:30:38 +0600

    crud done

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -11,26 +11,70 @@ export class EmployeesService {
 
   }
 
+  /**
+   * Create new employee
+   * @param createEmployeeDto 
+   * @returns 
+   */
   create(createEmployeeDto: Prisma.EmployeeCreateInput) {
     const item = this.databaseService.employee.create({
       data: createEmployeeDto
     });
     return item;
   }
+  /**
+   * Find all employee or filter by role
+   * @param role 
+   * @returns 
+   */
 
-  findAll() {
-    return `This action returns all employees`;
+  findAll(role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
+    
+    const employees = this.databaseService.employee.findMany({
+      where: {
+        role: role
+      }
+    })
+    return employees;    
   }
 
+  /**
+   * Find single employee by id
+   * @param id 
+   * @returns 
+   */
   findOne(id: number) {
-    return `This action returns a #${id} employee`;
+    const employee = this.databaseService.employee.findFirst({
+      where: {
+        id
+      }
+    });
+    return employee;    
   }
+
+  /**
+   * Update employee
+   * @param id 
+   * @param updateEmployeeDto 
+   * @returns 
+   */
 
   update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
-    return `This action updates a #${id} employee`;
+    const updated = this.databaseService.employee.update({
+      where: {
+        id: id
+      },
+      data: updateEmployeeDto
+    });
+    return updated;    
   }
 
   remove(id: number) {
-    return `This action removes a #${id} employee`;
+    const deleted = this.databaseService.employee.delete({
+      where: {
+        id
+      }
+    });
+    return deleted;
   }
 }
